3dbf873749278ca26766b94fd8bbc16b1affd2a3
adictya <aditya.26101@gmail.com>
2021-11-10 20:07:17 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,117 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import AgoraUIKit, { layout } from "agora-react-uikit";
+import { datadogLogs } from "@datadog/browser-logs";
 
-function App() {
+import "agora-react-uikit/dist/index.css";
+
+const App = () => {
+  const [videocall, setVideocall] = useState(true);
+  const [role, setRole] = useState(false);
+  const [isPinned, setPinned] = useState(false);
+
+  useEffect(() => {
+    datadogLogs.logger.info("Application Started", {
+      name: "Application",
+      id: 1234,
+    });
+    console.error("I am an error");
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <div style={styles.videoContainer}>
+        <div style={styles.nav}>
+          <button
+            style={styles.btn}
+            onClick={() => {
+              datadogLogs.logger.log("Log Button clicked from vercel", {
+                name: "Button",
+                id: 1234,
+              });
+            }}
+          >
+            Log this
+          </button>
+          <button
+            style={styles.btn}
+            onClick={() => {
+              datadogLogs.logger.warn("Warn Button clicked from vercel", {
+                name: "Button",
+                id: 1234,
+              });
+            }}
+          >
+            Warn this
+          </button>
+          <button
+            style={styles.btn}
+            onClick={() => {
+              datadogLogs.logger.error("Error Button clicked from vercel", {
+                name: "Button",
+                id: 1234,
+              });
+            }}
+          >
+            Error this
+          </button>
+        </div>
+        <h1 style={styles.heading}>Agora React Web UI Kit</h1>
+        {videocall ? (
+          <>
+            <div style={styles.nav}>
+              <p style={{ fontSize: 20, width: 200 }}>
+                You're {role ? "an audience" : "a host"}
+              </p>
+              <p style={styles.btn} onClick={() => setRole(!role)}>
+                Change Role
+              </p>
+              <p style={styles.btn} onClick={() => setPinned(!isPinned)}>
+                Change Layout
+              </p>
+            </div>
+            <AgoraUIKit
+              rtcProps={{
+                // appId: "1e6816ded05149088f32daa1c0d19456",
+                channel: "test",
+                role: role ? "audience" : "host",
+                layout: isPinned ? layout.pin : layout.grid,
+              }}
+              callbacks={{
+                EndCall: () => setVideocall(false),
+              }}
+            />
+          </>
+        ) : (
+          <div style={styles.nav}>
+            <h3 style={styles.btn} onClick={() => setVideocall(true)}>
+              Start Call
+            </h3>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flex: 1,
+    backgroundColor: "#007bff22",
+  },
+  heading: { textAlign: "center", marginBottom: 0 },
+  videoContainer: { display: "flex", flexDirection: "column", flex: 1 },
+  nav: { display: "flex", justifyContent: "space-around" },
+  btn: {
+    backgroundColor: "#007bff",
+    cursor: "pointer",
+    borderRadius: 5,
+    padding: 5,
+    color: "#ffffff",
+    fontSize: 20,
+  },
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { datadogLogs } from '@datadog/browser-logs'
+
+datadogLogs.init({
+  clientToken: 'pubf592f6fbe2d6dd79240b89e192e848cb',
+  site: 'datadoghq.com',
+  forwardErrorsToLogs: true,
+  sampleRate: 100,
+})
 
 ReactDOM.render(
   <React.StrictMode>
